693156aaaa6d17f5af8c78a4ad928314ff498813
Maretti Filippo <69721649+WeridFire@users.noreply.github.com>
2023-03-27 11:47:29 +0200

    Test connection function added

--- mappsite/helpers.py
+++ mappsite/helpers.py
@@ -1,6 +1,23 @@
-import requests
+import requests as rq
+
 def test_connection(website, dir_string):
     # check if dir_string is a valid link for website
+    # website form = "htttp://www.urltest.domain"
+    # dir_string form = "directory_name" 
+    
+    # makes the srings raw
+    website = r"{}".format(website)
+    dir_string = r"{}".format(dir_string)
+
+    # full url that will be tested
+    full_url = f"{website}/{dir_string}"
+
+    # testing full url
+    response = rq.get(full_url)
+    print(f"Testing {full_url}")
+    if response.status_code == 200:
+        return True
+    
     return False
 
 def tree_append(tree_structure, sub_dir, dir_string):
@@ -9,3 +26,4 @@ def tree_append(tree_structure, sub_dir, dir_string):
 def link_bruteforce(website, sub_dir_, tree_structure):
     # loop over possible subdirectories trying with all possible char combinations
     return
+
