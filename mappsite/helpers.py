194860102a2258fe73c42fb2be97dd6f0dda6f88
Giovanni Durante <giovanni3durante@gmail.com>
2023-03-29 11:30:43 +0200

    function templates

--- mappsite/helpers.py
+++ mappsite/helpers.py
@@ -1,29 +1,50 @@
 import requests as rq
+import copy
 
-def test_connection(website, dir_string):
+
+def test_connection(website, dir_string, tree_structure):
     # check if dir_string is a valid link for website
     # website form = "htttp://www.urltest.domain"
     # dir_string form = "directory_name" 
-    
+
     # makes the srings raw
     website = r"{}".format(website)
     dir_string = r"{}".format(dir_string)
-
-    # full url that will be tested
     full_url = f"{website}/{dir_string}"
 
     # testing full url
-    response = rq.get(full_url)
-    print(f"Testing {full_url}")
-    if response.status_code == 200:
+    response = rq.head(full_url, allow_redirects=True)
+
+    # 1xx - informational
+    # 2xx - success
+    # 3xx - redirection
+    # 4xx - client error and so on with errors
+    if not response.status_code < 400:
+        return False
+
+    # check redirections' status if they exist and save them -- error for invalid redirections
+    # see https://requests.readthedocs.io/en/latest/user/quickstart/#redirection-and-history
+    if not response.history:
         return True
-    
-    return False
+
+    # explore redirections
+    # |url 1| --> |url 2| --> |url 3| --> status code >= 400 STOP
+    chain = copy.deepcopy(full_url)
+    for resp in response.history:
+        if resp.status_code >= 400:
+            break
+        tree_append(tree_structure, chain, resp.url)
+        chain = resp.url
+
+    return True
+
 
 def tree_append(tree_structure, sub_dir, dir_string):
     # append dir_string as a valid resource to sub_dir in tree_structure
     return
-def link_bruteforce(website, sub_dir_, tree_structure):
-    # loop over possible subdirectories trying with all possible char combinations
-    return
 
+
+def link_bruteforce(website, sub_dir_, tree_structure, chars_num):
+    # loop over possible subdirectories trying with all possible char combinations
+
+    return
