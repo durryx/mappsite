7a4e34d12aec5e305482ed11274840cd8b2b0c8e
Giovanni Durante <giovanni3durante@gmail.com>
2023-04-14 18:43:22 +0200

    fixed helpers.py bugs

--- mappsite/helpers.py
+++ mappsite/helpers.py
@@ -20,41 +20,45 @@ def test_connection(website: str, dir_string: str):
     # 3xx - redirection
     # 4xx - client error and so on with errors
     if response.status_code >= 400:
-        return False
+        return False, None
 
     # check redirections' status if they exist and save them -- error for invalid redirections
     # see https://requests.readthedocs.io/en/latest/user/quickstart/#redirection-and-history
-    if not response.history:
-        return True
+    if len(response.history) == 1:
+        return True, None
 
     # explore redirections
     # |url 1| --> |url 2| --> |url 3| --> status code >= 400 STOP
     temp_tree = tr.Tree()
     temp_tree.create_node(full_url, full_url)
     chain = copy.deepcopy(full_url)
-    for resp in response.history:
+    for resp in response.history[1:]:
         if resp.status_code >= 400:
             break
         temp_tree.create_node(resp.url, resp.url, parent=chain)
         chain = resp.url
 
-    return [True, temp_tree]
+    return True, temp_tree
 
 
-def link_cat(website_fs: tr.Tree, node: tr.Node, link: str):
+def recursive_cat(website_fs: tr.Tree, node: tr.Node, link: str):
     if node.is_root():
         return link
     else:
         prev_node_id = node.predecessor(website_fs.identifier)
         prev_node = website_fs.get_node(prev_node_id)
         link = prev_node.tag + '/' + link
-        return link_cat(website_fs, prev_node, link)
+        return recursive_cat(website_fs, prev_node, link)
+
+
+def link_cat(website_fs: tr.Tree, node: tr.Node):
+    return recursive_cat(website_fs, node, '') + node.tag
 
 
 def handle_user_input(thread_pool):
     # while True:
-        # print and get relevant info
-        # check if thread has joined with thread_pool.shutdown(wait=True)
+    # print and get relevant info
+    # check if thread has joined with thread_pool.shutdown(wait=True)
     pass
 
 
--- test/test_helpers.py
+++ test/test_helpers.py
@@ -11,15 +11,15 @@ class TestHelpers(unittest.TestCase):
 
     def test_connection_redirections(self):
         url = "http://httpbin.org"
-        outcome, tree = mappsite.helpers.test_connection(url, "relative-redirect/2", )
+        outcome, tree = mappsite.helpers.test_connection(url, "relative-redirect/4", )
         self.assertEqual(outcome, True)
-        self.assertNotEquals(tree, None)
+        self.assertNotEqual(tree, None)
         tree.show()
 
     def test_link_cat(self):
         tree = tr.Tree()
-        tree.create_node("https://www.polimi.it/", "https://www.polimi.it/")
-        tree.create_node("il-politecnico", "il-politecnico", parent="https://www.polimi.it/")
+        tree.create_node("https://www.polimi.it", "https://www.polimi.it")
+        tree.create_node("il-politecnico", "il-politecnico", parent="https://www.polimi.it")
         tree.create_node("chi-siamo", "chi-siamo", parent="il-politecnico")
         tree.create_node("i-nostri-valori", "i-nostri-valori", parent="chi-siamo")
         tree.create_node("politica-per-la-qualita-di-ateneo", "politica-per-la-qualita-di-ateneo", parent="chi-siamo")
@@ -27,7 +27,7 @@ class TestHelpers(unittest.TestCase):
         tree.show()
 
         node = tree.get_node("polimi-2040")
-        full_link = mappsite.helpers.link_cat(tree, node, '')
+        full_link = mappsite.helpers.link_cat(tree, node)
         self.assertEqual(full_link, "https://www.polimi.it/il-politecnico/chi-siamo/polimi-2040")
 
 
