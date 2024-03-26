9d46985e1cf1b5e942623e6f3926953a0103d1cc
Andrea Sevincel <49096189+AndreaSevincel@users.noreply.github.com>
2024-03-26 20:26:48 +0100

    Update fast_mode.py
    extract_from_link + is_same_domain

--- mappsite/fast_mode.py
+++ mappsite/fast_mode.py
@@ -1,26 +1,59 @@
 import os
 import sys
-
+from bs4 import BeautifulSoup 
 file_dir = os.path.dirname(__file__)
 sys.path.append(file_dir)
 from base_class import *
 from helpers import *
+import requests
+import re
+
 
 
 class FastScan(WrapperScan):
+    global links
+    links = []
+    
 
     def handle_user_input(self):
         pass
 
-    def extract_from_link(self, link: str):
-        # extract and all urls present in the HTML code present in the link
-        # use function is_same_domain to check if the urls is not on the same domain
-        pass
+    def extract_from_link(self, link: str, url):
+      
+        response = requests.get(url) #json
+
+        global urlDaAcchiappare
+        urlDaAcchiappare = "www.polimi.it"
+        #porcocane
+  
+        htmlDoc = requests.get(urlDaAcchiappare) 
+  
+        soup = BeautifulSoup(htmlDoc, 'html.parser') 
+
+
+        # trovi tutti che hanno  con "href"  
+        # quelli che iniziano con https://
+        for link in soup.find_all('a',  
+            attributi={'href': re.compile("^https://")}): 
+            links.append(link.get('href'))
+        
+        return links
+
+
 
     def is_same_domain(self, link: str):
+        i = 0
+        for x in range(links):
+            if urlDaAcchiappare == links[i]:
+                link[i] = link[i][0: len(urlDaAcchiappare)]
+                i += 1
+                return
+            else:
+                del links[i]
+                i += 1
         # check if link is same domain as website_fs (accessible as self.website_fs.root())
         # return list with all urls
-        pass
+        return links
 
     def add_link_to_tree(self, link: str):
         # create data structure to efficiently add urls without searching in tree
