d2b4c6273f01a306b2f7650b84d13de30c1ada69
Giovanni Durante <giovanni3durante@gmail.com>
2024-03-29 00:15:41 +0100

    command line options

--- mappsite/fast_mode.py
+++ mappsite/fast_mode.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from bs4 import BeautifulSoup
+
 file_dir = os.path.dirname(__file__)
 sys.path.append(file_dir)
 from base_class import *
@@ -13,39 +14,37 @@ links = []
 
 class FastScan(WrapperScan):
 
-
-
     def handle_user_input(self):
         pass
+
     def extract_from_link(self, link: str, url):
 
-        response = requests.get(url) #json
+        response = requests.get(url)  # json
 
-        #making a request to a web page, in this case to website_fs
+        # making a request to a web page, in this case to website_fs
         htmlDoc = requests.get(self.website_fs.root())
 
-        #analyze the html of the website into its parts
+        # analyze the html of the website into its parts
         soup = BeautifulSoup(htmlDoc, 'html.parser')
-
 
         # find any content with the "href" tag
         # also starting with https:// but sometimes the website may start with http!!!
         for link in soup.find_all('a',
-            attributi={'href': re.compile("^https://")}):
+                                  attributi={'href': re.compile("^https://")}):
             links.append(link.get('href'))
-        #put everything into the list 'links'
+        # put everything into the list 'links'
         return links
-
 
     def is_same_domain(self, link: str):
         i = 0
 
         for x in range(links):
             for a in range(len(self.website_fs.root())):
-                #comparing every element from links with website_fs with the length of website_fs
-                #so if the website starts with https:// it will always compare the first 8 characters
+                # comparing every element from links with website_fs with the length of website_fs
+                # so if the website starts with https:// it will always compare the first 8 characters
                 if self.website_fs.root() == links[i]:
-                    link[i] = link[i][0: len(self.website_fs.root())] #the second parentheses is to compare only the website_fs length and not the rest
+                    link[i] = link[i][0: len(
+                        self.website_fs.root())]  # the second parentheses is to compare only the website_fs length and not the rest
 
                     i += 1
                     return
